16a296c446db90e0a34e7eec77df53f424792ad4
PattyDePuh <pattydpuh@gmail.com>
2015-10-22 17:46:04 +0200

    Einleseveorgang + GraphValidator done

--- src/header.hpp
+++ src/header.hpp
@@ -1,35 +1,63 @@
-//Klassen
-
+/* Header-File “header.hpp“: */
+#ifndef HEADER_HPP
+#define HEADER_HPP
 
 //Strukturen
 //Knoten im Graph
 struct knoten_konstrukt{
+	knoten_konstrukt(int id, int restriction): id(id), restriction(restriction) {};
 	int id;
 	int restriction;
 };
 //Kanten im Graph
 struct kanten_konstrukt{
+	kanten_konstrukt(int id, int start, int ziel, int kosten, int result): id(id), start(start), ziel(ziel), kosten(kosten), result(result) {};
 	int id;
 	int start;
 	int ziel;
 	int kosten;
+	int result;
 };
 
-typedef kanten_konstrukt kante;
-typedef knoten_konstrukt knoten;
+typedef kanten_konstrukt Kante;
+typedef knoten_konstrukt Knoten;
 
 //Variablen
+//Läufer
 int i;
 int k_id;
+
+//Parameter-Flag
+bool eval_parameter;
+string input_path;
+string output_path;
 
 //Hilfsvariablen zur Zeiterfassung
 clock_t start_zeit;
 clock_t end_zeit;
 
 //Graphenstacks
-vector<kante> kantenliste;
-vector<knoten> knotenliste;
+vector<Kante> kantenliste;
+vector<Knoten> knotenliste;
+
+//Einlese-Stream
+ifstream input_graph;
+ofstream output_graph;
+
+//Zähler
+int anzahl_knoten;
+int anzahl_kanten;
+
+//Fehlerausgabe
+std::stringstream fehlertext;
+bool erfolg;
 
 //Methoden von main.c
 void showUsage(void);
-void einlesen(char* path);
+void einlesen(string path);
+bool validate();
+bool optimize();
+void ausgeben(string path);
+
+#endif 
+/* Ende HEADER_HPP */
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,22 @@
 #include <stdio.h>
 #include <time.h>
+#include <stdlib.h>
 #include <vector>
+#include <istream>
+#include <iostream>
 #include <string>
+#include <cstring>
+#include <fstream>
+#include <sstream>
+#include <exception>
+#include <queue>
 
 using std::vector;
+using std::ifstream;
+using std::ofstream;
+using std::string;
+using std::strtok;
+using std::queue;
 
 #include "header.hpp"
 
@@ -16,35 +29,205 @@ int main(int argc, char* argv[]){
   	showUsage();
   	return 1;
   }
-
-  //Vorbereitung
-  
-	printf("Moin, dies ist der Aufruf von r-spantree.\n");
-
-  // hier einlesen
+  printf("Moin, dies ist der Aufruf von r-spantree.\n");
+  //Parameter filtern
+  eval_parameter = false;
   for( i = 1; i < argc - 1; i++ ){
-    if(argv[i] == "-in"){
-      einlesen(argv[i+1]);
+    //Argument erfassen
+    string s = argv[i];
+    //Ist der -in Parameter erfasst, lese die Datei aus folgenden Path ein.
+    if(s.compare( "-in" ) == 0){
+      input_path = argv[i+1];
+    }
+    if(s.compare( "-out" ) == 0){
+      output_path = argv[i+1];
+    }
+    //Ist der -eval Parameter erfasst, setze die entsprechende flag 
+    if(s.compare( "-eval" ) == 0){
+      eval_parameter = true;
+      input_path = argv[i+1];
     }
   }
-  
-  //Start
-  start_zeit = clock();
+    
+  //Parameter Bearbeitung/Validierung
+  einlesen(input_path);
+  if(eval_parameter == true){
+    //Führe Validierung
+    erfolg = validate();
+    if(erfolg){
+      //Validierung erfolgreich
+      printf("valide Lösung\n");
+      return 0;
+    }else{
+      //Lösung ist nicht valide.
+      printf("ERROR:\n");
+      std::cout << fehlertext.str();
+      return 1;
+    }
+  }else{
+    //Führe Optimierung durch
+    erfolg = optimize();
+    if(erfolg){
+      ausgeben(output_path);
+    }
+  }
 
-  //TODO hier ausarbeiten
-
-  end_zeit = clock();
-
-  //Ausgabe
+  //TODO dynamische Ausgabe 
   printf("%d\t%ld\n", -1, ((end_zeit - start_zeit) * 1000) / CLOCKS_PER_SEC );
   return 0;
 }
+
+
 
 void showUsage(){
 	printf("Aufrufschema: './rspantree -in <path/to/input> -out <path/to/output>'\n");
 	printf("Bzw: './r-spantree -eval <path/to/instance>' für Evaluierung");
 }
 
-void einlesen(char* path){
-  printf("Ich lese ein von %s ...\n", path);
+
+
+void einlesen(string path){
+  std::cout << "Ich lese ein von "<< path << " ...\n";
+  //Initialisierung Stream
+  input_graph.open(path);
+  //Lese Kanten und Knotenanzahl aus.
+  string input_knoten;
+  string input_kanten;
+  getline(input_graph, input_knoten);
+  getline(input_graph, input_kanten);
+  anzahl_knoten = stoi(input_knoten);
+  anzahl_kanten = stoi(input_kanten);
+  //Reserviere Speicher in den Stacks
+  kantenliste.reserve(anzahl_kanten);
+  knotenliste.reserve(anzahl_knoten);
+  //Lese die Knoten ein
+  for(int j = 0; j <= anzahl_knoten; j++){
+    string input_string;
+    getline(input_graph, input_string);
+    knotenliste.emplace_back(j, stoi(input_string));
+  }
+  //Lese die Kanten ein
+  for(int j = 0; j <= anzahl_knoten; j++){
+    //Trenne die eingelesene Zeile an den Leerzeichen.
+    string input_string;
+    //Startknoten
+    getline(input_graph, input_string, ' ');
+    int stk = stoi(input_string);
+
+    //Endknoten
+    getline(input_graph, input_string, ' ');
+    int enk = stoi(input_string);
+
+    //Kosten
+    getline(input_graph, input_string, ' ');
+    int cost = stoi(input_string);
+
+    //ggf. Lösungsvorschlag
+    getline(input_graph, input_string);
+    int result = 0;
+    if(stoi(input_string) == 1){
+      result = 1;
+    }
+    kantenliste.emplace_back(j, stk, enk, cost, result);
+  }
+  input_graph.close();
+  printf("Einlese-Prozess fertig.\n");
+}
+
+
+
+bool optimize(){
+  //Start
+  start_zeit = clock();
+
+  //TODO hier ausarbeiten
+
+  //Ende
+  end_zeit = clock();
+
+  //TODO guter return
+  return false;
+}
+
+
+
+bool validate(){
+  bool valide = true;
+  //Initialisierung des Check-Arrays und des Stacks
+  bool besucht[anzahl_knoten];
+  for (int i = 0; i < anzahl_knoten; i++) {
+    besucht[i] = false;
+  }
+  queue<int> such_que;
+  //Überprüfe durch Breiten-Suche, ob von einem Knoten alle anderen Knoten erreichbar sind.
+  //Beginne mit Knoten Nr 1;
+  such_que.push(0);
+  //Wiederhole folgenden Vorgang, bis es keine lokalen Orte mehr existieren.
+  do{
+    //Hol dir einen Knoten aus der Such-Que
+    Knoten n = knotenliste.at(such_que.front());
+    such_que.pop();
+    //Markiere den Knoten als besucht.
+    besucht[n.id] = true;
+
+    //Debug-Array Ausgabe
+    #ifdef DEBUG
+    string test = "[";
+    for(int x = 0; x < anzahl_knoten; x++){
+      if(besucht[x] == true){
+        test.append("o");
+      }else{
+        test.append("x");
+      }
+    }
+    test.append("]");
+    std::cout << test << "\n";
+    #endif
+
+    int incidente = 0;
+    //Hol die Kanten, die von dem Knoten ausgehen,
+    for (std::vector<Kante>::iterator kante = kantenliste.begin() ; kante != kantenliste.end(); ++kante){
+      //Lese Eingangs/Ausgangskante aus.
+      if(kante->start == n.id){
+        //Wenn eine incidente Kante, zähle hoch
+        if(kante->result = 1){
+          ++incidente;
+        }
+        //Schreibe Nachbarkante in die Que, wenn noch nicht besucht
+        if(besucht[kante->ziel] == false){
+          such_que.push(kante->ziel);
+        }
+      }
+      if(kante->ziel == n.id){
+        //Wenn eine incidente Kante, zähle hoch
+        if(kante->result = 1){
+          ++incidente;
+        }
+        //Schreibe Nachbarkante in die Que, wenn noch nicht besucht
+        if(besucht[kante->start] == false){
+          such_que.push(kante->start);
+        }
+      }
+    }
+    //Abschließend überprüfen, dass die Anzahl Kanten
+    if(n.restriction < incidente){
+      fehlertext << "Knoten Nr." << n.id << " hat zuviele Kanten: " << incidente << " von " << n.restriction << "\n";
+      valide == false;
+    }
+  } while(!such_que.empty());
+
+  //Wenn keine Restriction gebrochen wurde, 
+  //überprüfe im Array, ob alle Knoten besucht wurden.
+  for(i = 0; i < anzahl_knoten; i++){
+    if(besucht[i] == false){
+      fehlertext << "Knoten Nr." << i << " ist vom Knoten Nr.0 nicht erreichbar!\n"; 
+      valide = false;
+    }
+  }
+
+  return valide;
+}
+
+void ausgeben(string path){
+  //TODO
 }
