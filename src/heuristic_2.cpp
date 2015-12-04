2ad1c010ff2bcca20677f139fb792da6e6809899
Michael Stypa <michael@stypa.info>
2015-12-04 18:36:18 +0100

    wip

--- src/heuristic_2.cpp
+++ src/heuristic_2.cpp
@@ -5,6 +5,8 @@
 #include "header.hpp"
 #include "heuristic_2.hpp"
 
+vector<Kante> tmpedges;
+
 bool edgeCompare (Kante l, Kante r) {
   return l.cost < r.cost;
 }
@@ -14,13 +16,17 @@ bool circleRecursive(int nodeIndex, std::vector<Knoten>* nodes, std::vector<Kant
     return false;
   }
   if (visited[nodeIndex]) {
+    std::cout << "circle at node " << nodeIndex << " from " << sourceIndex << std::endl;
     return true;
   }
   visited[nodeIndex]++;
 
-  for (std::vector<Kante>::iterator it = edges->begin(); it < edges->end(); it++) {
-    if (it->start == nodesIndex && it->ziel) {
-      circleRecursive(it, edges, visited, finished, node);
+  for (std::vector<Kante>::iterator it = edges.begin(); it < edges.end(); it++) {
+    if (it->start == nodeIndex && it->ziel != sourceIndex) {
+      circleRecursive(it->ziel, nodes, edges, visited, finished, nodeIndex);
+    }
+    if (it->ziel == nodeIndex && it->start != sourceIndex) {
+      circleRecursive(it->start, nodes, edges, visited, finished, nodeIndex);
     }
   }
 
@@ -29,8 +35,8 @@ bool circleRecursive(int nodeIndex, std::vector<Knoten>* nodes, std::vector<Kant
 }
 
 bool circle (std::vector<Knoten>* nodes, std::vector<Kante>& edges) {
-  int visited[nodes.size()] = {};
-  int finished[nodes.size()] = {};
+  int visited[nodes->size()] = {};
+  int finished[nodes->size()] = {};
   for (std::vector<Knoten>::iterator it = nodes->begin(); it < nodes->end(); it++) {
     circleRecursive(it->id, nodes, edges, visited, finished, it->id);
   }
@@ -40,12 +46,12 @@ bool circle (std::vector<Knoten>* nodes, std::vector<Kante>& edges) {
 long optimize (std::vector<Knoten>* nodes, std::vector<Kante>* edges, int, int) {
   //TODO think about sorting the other way
   std::sort(edges->begin(), edges->end(), edgeCompare);
-  std::reverse(edges.begin(), edges.end());
+  std::reverse(edges->begin(), edges->end());
 
-  while (edges.size()) {
-    Kante* e = edges.end();
-    result.push_back(e);
-    if (circle(nodes, result));
+  while (edges->size()) {
+    tmpedges.push_back(*(edges->end()));
+    edges->pop_back();
+    circle(nodes, tmpedges);
   }
 
   return -1;
--- src/heuristic_2.hpp
+++ src/heuristic_2.hpp
@@ -1,10 +1,8 @@
 #ifndef HEURISTIC_2_HPP
 #define HEURISTIC_2_HPP
 
-vector<Edge> result;
-
 // comparator deciding if lhs has less cost than rhs
-bool edgeCompare (Edge, Edge);
+bool edgeCompare (Kante, Kante);
 
 // tries to find path with minimal cost using modified Kruskal
 long optimize (vector<Knoten>*, vector<Kante>*, int, int);
